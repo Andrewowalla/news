fb5f02418fd85ca1f1d2d212bead1924af294ec1
Andrewowalla <andrewowalla@gmail.com>
2022-01-31 19:46:25 +0300

    update __init__.py to create our application facory function

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,17 @@
 from flask import Flask 
-from .config import DevConfig
+from config import config_options
 
-#initializing application
-app = Flask(__name__)
- from app import views
+bootsratp = Bootstrap()
+
+def create_app('config_name'):
+
+    app = Flask(__name__)
+
+    #creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    #initializing flask extensions
+    bootstrap.init_app(app)
+    
+    from app import views
+    return app
