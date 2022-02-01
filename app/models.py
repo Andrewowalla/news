add685eba2f7478a56e9e2bd21cb92c9da6dcc73
Andrewowalla <andrewowalla@gmail.com>
2022-02-01 09:09:10 +0300

    change the already created class news_source to news_article

--- app/models.py
+++ app/models.py
@@ -2,6 +2,14 @@ class news_source:
     '''
     news_source class to define news source objects
     '''
+    def __init__(self, id, name):
+        self.id = id
+        self.
+
+class news_article:
+    '''
+    news_article class to define news article objects
+    '''
     def __init__(self, title, description, url, urlToImage, content, publishedAt):
         self.title = title
         self.description = description
