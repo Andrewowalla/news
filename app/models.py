f15349e8552a1ff341fb7c8d96d22d7d9c4f6a40
Andrewowalla <andrewowalla@gmail.com>
2022-02-01 08:38:49 +0300

    create class new_source

---
+++ app/models.py
@@ -0,0 +1,11 @@
+class news_source:
+    '''
+    news_source class to define news source objects
+    '''
+    def __init__(self, title, description, url, urlToImage, content, publishedAt):
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.content = content
+        self.publishedAt = publishedAt
